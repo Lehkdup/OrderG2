a81c81122c4ebe5fb4ee853f0adc3d127ca439c3
Lehkdup <s.lehkdup@gmail.com>
2024-11-29 10:57:17 +0100

    Buch Suche Implementiert

--- src/main/java/com/example/order/controller/BookController.java
+++ src/main/java/com/example/order/controller/BookController.java
@@ -7,32 +7,56 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 import org.slf4j.Logger;
 
 @Controller
 public class BookController {
-    private final RestTemplate restTemplate;
+    private final RestClient restClient;
     private static final Logger logger = LoggerFactory.getLogger(BookController.class);
 
-    public BookController(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
+    public BookController(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.build();
     }
 
-    private Book[] getBook(String keywords) {
-        String url = "http://localhost:8080/books/search/" + keywords;
+    public Book[] getBook(String keywords) {
+        String url;
+        if (keywords == null || keywords.isEmpty()) {
+            url = "http://localhost:8080/books";
+            logger.info("Aufruf der URL: {}", url);
+        }else{
+            url = "http://localhost:8080/books/search/" + keywords;
+            logger.info("http://localhost:8080/books/search/" + keywords);
+        }
+
         try {
-            return restTemplate.getForObject(url, Book[].class);
+            return restClient.get()
+                    .uri(url)
+                    .retrieve()
+                    .body(Book[].class);
         } catch (Exception e) {
             logger.error("Fehler beim Abrufen der Bücher von {}: {}", url, e.getMessage());
             return new Book[0];
         }
     }
 
+
+    @GetMapping("/")
+    public String catalog() {
+        return "index"; // Name der Datei ohne `.html`, wenn du Thymeleaf nutzt
+    }
+    /*
     @GetMapping("/search")
-    public String SearchBook(Model model, @RequestParam String keywords) {
+    public String SearchBook(Model model, String keywords) {
         model.addAttribute("books", getBook(keywords));
         model.addAttribute("keywords", keywords); // Keywords dem Model hinzufügen
+        return "index";
+    }*/
+    @GetMapping("/search")
+    public String SearchBook(Model model, String keywords) {
+
+        model.addAttribute("books", getBook(keywords));
         return "index";
     }
 
